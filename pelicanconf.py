f6cc06b205f015c768257125a2c56a4a3d50edf1
Jon Chen <bsd@voltaire.sh>
2014-06-06 01:06:58 -0400

    update pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = 'bsdlp'
 SITENAME = 'voltaireMC Blog'
-SITEURL = ''
+SITEURL = 'http://mc.voltaire.sh/blog'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Etc/UTC'
 
 DEFAULT_LANG = 'en'
 
@@ -14,18 +14,6 @@ DEFAULT_LANG = 'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
